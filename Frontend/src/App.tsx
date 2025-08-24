7965c447857f79b956f085206000f8bd06161b01
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-24 12:30:47 +0530

    landing page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/landing";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ Frontend/src/pages/landing.tsx
@@ -0,0 +1,43 @@
+import "../App.css";
+import { Link } from "react-router-dom";
+
+export default function LandingPage() {
+  return (
+    <div className="landingPageContainer">
+      <nav>
+        <div className="navHeader">
+          <img
+            src="/public/logo_connect.svg"
+            alt="Logo"
+            height="100px"
+            width="200px"
+          />
+        </div>
+        <div className="navlist">
+          <p>Join as guest</p>
+          <p>Register</p>
+
+          <Link to="/home" className="loginBtn">
+            <p>Login</p>
+          </Link>
+        </div>
+      </nav>
+
+      <div className="landingMainContainer">
+        <div>
+          <h1 className="heading">
+            Limitless connection and collaboration <br /> with ConnectRight
+          </h1>
+
+          <Link to="/home" className="loginBtn">
+            <p>Get Started</p>
+          </Link>
+        </div>
+
+        <div className="heroImg">
+          <img src="/public/hero.png" alt="Hero" height="500px" width="800px" />
+        </div>
+      </div>
+    </div>
+  );
+}
